4b3a66e4228674d41be1fc8cfe0d56201731648e
tanphuccgl <phamnguyentanphucgl@gmail.com>
2022-10-21 02:45:06 +0700

    update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,30 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+
 
 void main() {
+  group('MyWidget', () {
+    testWidgets('should display a string of text', (WidgetTester tester) async {
+      // Define a Widget
+      const myWidget = MaterialApp(
+        home: Scaffold(
+          body: Text('Hello'),
+        ),
+      );
+
+      // Build myWidget and trigger a frame.
+      await tester.pumpWidget(myWidget);
+
+      // Verify myWidget shows some text
+      expect(find.byType(Text), findsOneWidget);
+    });
+  });
 }
